bb8143c5ad852835c60827e21e5cb5af6a38e293
satin <satin3737@gmail.com>
2026-04-29 21:34:24 +0300

    First commit

---
+++ .prettierrc.ts
@@ -0,0 +1,24 @@
+import type {Config} from 'prettier';
+
+const config: Config = {
+    $schema: 'https://json.schemastore.org/prettierrc',
+    semi: true,
+    tabWidth: 4,
+    singleQuote: true,
+    printWidth: 120,
+    trailingComma: 'none',
+    bracketSpacing: false,
+    arrowParens: 'avoid',
+    importOrder: [
+        '<THIRD_PARTY_MODULES>',
+        '^@/types(.*)$',
+        '^@/(.*)$',
+        '^[./(.*)$]',
+        '^(?!.*\\.module\\.scss$)[./].*$',
+        '.*\\.module\\.scss$'
+    ],
+    importOrderSortSpecifiers: true,
+    plugins: ['@trivago/prettier-plugin-sort-imports']
+};
+
+export default config;
---
+++ eslint.config.ts
@@ -0,0 +1,45 @@
+import type {Linter} from 'eslint';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import {defineConfig, globalIgnores} from 'eslint/config';
+
+const customEsLintSettings: Linter.RulesRecord = {
+    '@typescript-eslint/no-unused-expressions': ['error', {allowShortCircuit: true, allowTernary: true}],
+    '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_', args: 'after-used', ignoreRestSiblings: true}
+    ],
+    '@typescript-eslint/no-explicit-any': ['error', {ignoreRestArgs: true}],
+    '@typescript-eslint/no-duplicate-enum-values': 'off',
+    '@typescript-eslint/member-ordering': [
+        'warn',
+        {
+            default: [
+                'static-field',
+                'public-instance-field',
+                'protected-instance-field',
+                'private-instance-field',
+                'constructor',
+                ['public-get', 'public-set'],
+                'public-method',
+                ['protected-get', 'protected-set'],
+                'protected-method',
+                ['private-get', 'private-set'],
+                'private-method'
+            ]
+        }
+    ]
+};
+
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+    {
+        languageOptions: {
+            rules: customEsLintSettings
+        }
+    }
+]);
+
+export default eslintConfig;
