db21d401fd1e76b420858cee4c009c62f018304a
satin <satin3737@gmail.com>
2026-05-03 18:27:04 +0300

    Removed unused schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,1 @@
 export * from './product-schema';
-export * from './upc-response-schema';
-export * from './go-upc-response-schema';
-export * from './upcdatabase-response-schema';
--- schemas/upcdatabase-response-schema.ts
+++
@@ -1,7 +0,0 @@
-import {z} from 'zod';
-
-export const upcDatabaseResponseSchema = z.object({
-    title: z.string().optional()
-});
-
-export type IUpcDatabaseResponse = z.infer<typeof upcDatabaseResponseSchema>;
